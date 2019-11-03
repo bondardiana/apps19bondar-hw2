19e01732f043de6540cf144215e94dc05f38aab7
Diana Bondar <45092495+bondardiana@users.noreply.github.com>
2019-11-03 20:13:28 +0200

    Update ImmutableArrayListTest.java

--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -27,7 +27,7 @@ public class ImmutableArrayListTest {
     }
 
 
-    @Test
+     @Test
     public void testImmutableArrayLListWithEls() {
         assertEquals(1, newArrayLList2.elements[0]);
         assertEquals(4, newArrayLList2.size());
@@ -37,28 +37,21 @@ public class ImmutableArrayListTest {
         assertEquals(3, newArrayLList2.get(3));
         Object [] t ={111, 222};
         Object [] what_I_want ={1, 2,22,3,111,222};
-        
-        newArrayLList2 = newArrayLList2.add(t[0]);
-        newArrayLList2 = newArrayLList2.add(t[1]);
-        
+        newArrayLList2=newArrayLList2.add(t[0]);
+        newArrayLList2=newArrayLList2.add(t[1]);
         assertArrayEquals(newArrayLList2.toArray(), what_I_want);
         Object [] what_I_want2 = {1,111,222, 2,22,3,111,222};
-      newArrayLList2=newArrayLList2.add(1,t[0]);
-        newArrayLList2=newArrayLList2.add(1,t[1]);
+        newArrayLList2=newArrayLList2.add(1,t[0]);
+        newArrayLList2=newArrayLList2.add(2, t[1]);
 
         assertArrayEquals(newArrayLList2.toArray(), what_I_want2);
         Object [] what_I_want3 = {9,111,222, 2,22,3,111,222};
 //        newLinkedList2=newLinkedList2.set(0,9);
 //        assertArrayEquals(newLinkedList2.toArray(), what_I_want3);
         assertEquals(newArrayLList2.indexOf(111), 1);
-//        newArrayLList2= newArrayLList2.clear();
-        assertNull(newArrayLList2.elements[0]);
-        assertEquals(newArrayLList2.isEmpty(), true);
-        assertEquals(newArrayLList2.size(), 0);
 
 
     }
-
 
 }
 
